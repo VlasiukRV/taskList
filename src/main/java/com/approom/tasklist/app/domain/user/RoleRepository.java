5b9df5081e9e36792df017e3fce55c24767768c6
Roma <vlasiukrv@gmail.com>
2017-07-28 14:26:21 +0300

    - refactor

--- src/main/java/com/approom/tasklist/app/domain/user/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.approom.tasklist.app.domain.user;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends CrudRepository<Role, Integer> {
-}
